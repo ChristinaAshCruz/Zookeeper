ba8e8963aa2c48de9c4d51fd30650907d2c246d6
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-22 14:52:10 -0800

    initial commit

---
+++ src/main/Mammal.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Mammal {
+	private Integer energyLevel = 100;
+	
+	public Mammal() {}
+
+	public Integer getEnergyLevel() {
+		return energyLevel;
+	}
+
+	public void setEnergyLevel(Integer energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+
+	
+}
